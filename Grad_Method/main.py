f04d9bc65e5ce14aa4fbcafadb78e2ff8a2bc570
Analyst Huang <13501637840@163.com>
2024-12-14 16:53:47 +0800

    delete unnecessary files

--- Grad_Method/main.py
+++
@@ -1,9 +0,0 @@
-import hydra
-from omegaconf import DictConfig, OmegaConf
-
-@hydra.main(config_path="conf", config_name="config")
-def run(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
-
-if __name__ == "__main__":
-    run()
